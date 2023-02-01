ae79531f6f4638cb17299547434d8fe623f2a6b0
Tayfun Oztemel <tayfun.oztemel@lunatech.nl>
2023-02-01 22:51:57 +0100

    check recursive functional list exercises

--- src/main/scala/fpinscala/exercises/datastructures/List.scala
+++ src/main/scala/fpinscala/exercises/datastructures/List.scala
@@ -43,15 +43,30 @@ object List: // `List` companion object. Contains functions for creating and wor
       case Nil => acc
       case Cons(x, xs) => f(x, foldRight(xs, acc, f))
 
+  // stack safe
+  def foldRightViaFoldLeft[A, B](as: List[A], acc: B, f: (A, B) => B): B =
+    foldLeft(reverse(as), acc, (b, a) => f(a, b))
+
   def sumViaFoldRight(ns: List[Int]): Int =
     foldRight(ns, 0, (x,y) => x + y)
 
+  /**
+    * Question:
+    * “Can product, implemented using foldRight, immediately halt the recursion and return 0.0 if it encounters a 0.0?
+    * Why or why not? Consider how any short-circuiting might work if you call foldRight with a large list.
+    * <p>
+    * Answer:
+    * “No, this is not possible.
+    * foldRight recurses all the way to the end of the list before invoking the function.
+    * A full traversal has occurred before the supplied function is ever invoked.
+    *
+    */
   def productViaFoldRight(ns: List[Double]): Double =
     foldRight(ns, 1.0, _ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
   def tail[A](l: List[A]): List[A] = l match {
     case List.Nil => sys.error("cannot return tail for Empty list")
-    case List.Cons(_: A, t: List[A]) => t
+    case List.Cons(_, t) => t
   }
 
   def setHead[A](l: List[A], h: A): List[A] =
@@ -61,16 +76,16 @@ object List: // `List` companion object. Contains functions for creating and wor
 
   @tailrec
   def drop[A](l: List[A], n: Int): List[A] =
-    l match
+    if n <= 0 then l
+    else l match
       case List.Nil => Nil
-      case ht@List.Cons(_, _) if n <= 0 => ht
       case List.Cons(_, t) => drop(t, n - 1)
 
   @tailrec
   def dropWhile[A](l: List[A], f: A => Boolean): List[A] =
     l match
-      case List.Nil => Nil
-      case a@ List.Cons(h, t) => if !f(h) then a else dropWhile(t, f)
+      case List.Cons(h, t) if f(h) => dropWhile(t, f)
+      case _ => l
 
   /**
     * returns a List consisting of all but the last element of a List.
@@ -99,53 +114,66 @@ object List: // `List` companion object. Contains functions for creating and wor
     foldLeft(ns, 0, _ + _)
 
   def productViaFoldLeft(ns: List[Double]): Double =
-    foldLeft(ns, 1, _ * _)
+    foldLeft(ns, 1.0, _ * _)
 
   def lengthViaFoldLeft[A](l: List[A]): Int =
-    foldLeft(l, 0, (x, _) => x + 1)
+    foldLeft(l, 0, (acc, _) => acc + 1)
 
   def reverse[A](l: List[A]): List[A] =
-    foldLeft(l, List.Nil : List[A], (x, y) => List.append(List(y), x))
+    foldLeft(l, Nil: List[A], (acc, a) => Cons(a, acc))
 
   def appendViaFoldRight[A](l: List[A], r: List[A]): List[A] =
-    foldRight(l, r, (a:A, b:List[A]) => List.Cons(a, b))
+    foldRight(l, r, Cons(_, _))
 
   def concat[A](l: List[List[A]]): List[A] =
-    foldLeft(
-      l,
-      List.Nil : List[A],
-      appendViaFoldRight(_, _)
-    )
+    foldRight(l, Nil: List[A], append)
+//    foldLeft(
+//      l,
+//      List.Nil : List[A],
+//      appendViaFoldRight(_, _)
+//    )
 
   def incrementEach(l: List[Int]): List[Int] =
-    foldRight(l, List.Nil : List[Int], (a:Int, acc: List[Int]) => Cons(a+1, acc))
+    foldRight(l, Nil : List[Int], (a, acc) => Cons(a+1, acc))
 
   def doubleToString(l: List[Double]): List[String] =
-    foldRight(l, List.Nil : List[String], (a, acc) => Cons(a.toString, acc))
+    foldRight(l, Nil : List[String], (a, acc) => Cons(a.toString, acc))
 
   def map[A, B](l: List[A], f: A => B): List[B] =
-    foldRight(l, List.Nil : List[B], (a, acc) => Cons(f(a), acc))
+    foldRight(l, Nil : List[B], (a, acc) => Cons(f(a), acc))
 
   def filter[A](as: List[A], f: A => Boolean): List[A] =
-    foldRight(as, List.Nil : List[A], (a, acc) => if f(a) then Cons(a, acc) else acc)
+    foldRight(as, Nil : List[A], (a, acc) => if f(a) then Cons(a, acc) else acc)
 
   def flatMap[A, B](as: List[A], f: A => List[B]): List[B] =
-    foldRight(as, List.Nil: List[B], (a, acc) => appendViaFoldRight(f(a), acc))
+    foldRight(as, Nil: List[B], (a, acc) => append(f(a), acc))
+
+  def flatMapViaConcatMap[A, B](as: List[A], f: A => List[B]): List[B] =
+    concat(map(as,f))
 
   def filterViaFlatMap[A](as: List[A], f: A => Boolean): List[A] =
-    flatMap(as, a => if f(a) then List(a) else List.Nil : List[A])
+    flatMap(as, a => if f(a) then List(a) else Nil)
 
   def addPairwise(a: List[Int], b: List[Int]): List[Int] =
     (a, b) match
-      case (Nil, _)=> Nil
-      case (_, Nil)=> Nil
-      case (Cons(ha, ta), Cons(hb, tb)) => Cons(ha+hb, addPairwise(ta, tb))
+      case (Nil, _) => Nil
+      case (_, Nil) => Nil
+      case (Cons(ha, ta), Cons(hb, tb)) => Cons(ha + hb, addPairwise(ta, tb))
 
   def zipWith[A, B, C](a: List[A], b: List[B], f: (A, B) => C): List[C] =
     (a, b) match
       case (Nil, _)=> Nil
       case (_, Nil)=> Nil
       case (Cons(ha, ta), Cons(hb, tb)) => Cons(f(ha,hb), zipWith(ta, tb, f))
+
+  def zipWithStacksafe[A, B, C](a: List[A], b: List[B], f: (A, B) => C): List[C] =
+    @tailrec
+    def loop(a: List[A], b: List[B], acc: List[C]): List[C] =
+      (a, b) match
+        case (Nil, _) => acc
+        case (_, Nil) => acc
+        case (Cons(ha, ta), Cons(hb, tb)) => loop(ta, tb, Cons(f(ha, hb), acc))
+    reverse(loop(a, b, Nil))
 
   def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean =
     (sup, sub) match
@@ -154,3 +182,16 @@ object List: // `List` companion object. Contains functions for creating and wor
       case (Cons(head, tail), Cons(sh, st)) =>
         if head == sh then hasSubsequence(tail, st)
         else hasSubsequence(tail, sub)
+
+  @tailrec
+  def startsWith[A](l: List[A], prefix: List[A]): Boolean = (l, prefix) match
+    case (_, Nil) => true
+    case (Cons(h1, t1), Cons(h2, t2)) if h1 == h2 => startsWith(t1, t2)
+    case _ => false
+
+  @tailrec
+  def hasSubsequenceStacksafe[A](sup: List[A], sub: List[A]): Boolean =
+    sup match
+      case Nil => sub == Nil
+      case _ if startsWith(sup, sub) => true
+      case Cons(h, t) => hasSubsequenceStacksafe(t, sub)
--- src/main/scala/fpinscala/exercises/datastructures/Tree.scala
+++ src/main/scala/fpinscala/exercises/datastructures/Tree.scala
@@ -1,5 +1,7 @@
 package fpinscala.exercises.datastructures
 
+import fpinscala.exercises.datastructures.Tree.Branch
+
 enum Tree[+A]:
   case Leaf(value: A)
   case Branch(left: Tree[A], right: Tree[A])
@@ -8,17 +10,26 @@ enum Tree[+A]:
     case Leaf(_) => 1
     case Branch(l, r) => 1 + l.size + r.size
 
-  def depth: Int = ???
+  def depth: Int = this match
+    case Leaf(_) => 0
+    case Branch(l, r) => 1 + l.depth.max(r.depth)
 
-  def map[B](f: A => B): Tree[B] = ???
+  def map[B](f: A => B): Tree[B] = this match
+    case Leaf(a) => Leaf(f(a))
+    case Branch(l, r) => Branch(l.map(f), r.map(f))
 
-  def fold[B](f: A => B, g: (B,B) => B): B = ???
+  def fold[B](f: A => B, g: (B,B) => B): B = this match
+    case Leaf(a) => f(a)
+    case Branch(l, r) => g(l.fold(f, g), r.fold(f, g))
   
-  def sizeViaFold: Int = ???
+  def sizeViaFold: Int =
+    fold(_ => 1, _ + _ + 1)
   
-  def depthViaFold: Int = ???
-  
-  def mapViaFold[B](f: A => B): Tree[B] = ???
+  def depthViaFold: Int =
+    fold(_ => 0, (l,r)=> 1 + (l max r))
+
+  def mapViaFold[B](f: A => B): Tree[B] =
+    fold(f.andThen(Leaf.apply), Branch(_, _))
 
 object Tree:
 
@@ -26,8 +37,14 @@ object Tree:
     case Leaf(_) => 1
     case Branch(l,r) => 1 + size(l) + size(r)
 
-  extension (t: Tree[Int]) def firstPositive: Int = ???
+  extension (t: Tree[Int])
+    def firstPositive: Int = t match
+      case Leaf(value) => value
+      case Branch(l, r) =>  if l.firstPositive > 0 then l.firstPositive else r.firstPositive
 
-  extension (t: Tree[Int]) def maximum: Int = ???
+    def maximum: Int = t match
+      case Leaf(value) => value
+      case Branch(left, right) => left.maximum.max(right.maximum)
 
-  extension (t: Tree[Int]) def maximumViaFold: Int = ???
+    def maximumViaFold: Int =
+      t.fold(a => a, _ max _)
