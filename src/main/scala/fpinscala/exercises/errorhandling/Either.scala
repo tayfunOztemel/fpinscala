c5e792306c0909f10a321c6dda6f28f00789825f
Tayfun Oztemel <tayfun.oztemel@lunatech.nl>
2023-02-21 13:59:29 +0100

    error handling

--- src/main/scala/fpinscala/exercises/errorhandling/Either.scala
+++ src/main/scala/fpinscala/exercises/errorhandling/Either.scala
@@ -8,26 +8,43 @@ enum Either[+E,+A]:
   case Left(get: E)
   case Right(get: A)
 
-  def map[B](f: A => B): Either[E, B] = ???
+  def map[B](f: A => B): Either[E, B] =
+    this match
+      case Right(a) => Right(f(a))
+      case Left(e) => Left(e)
 
-  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] =
+    this match
+      case Right(a) => f(a)
+      case Left(e) => Left(e)
 
-  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] =
+    this match
+      case Left(e) => b
+      case Right(r) => Right(r)
 
-  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    for {
+      aa <- this
+      bb <- b
+    } yield f(aa, bb)
 
 object Either:
-  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = ???
+  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+    es match
+      case ::(head, next) => f(head).map2(traverse(next)(f))(_ :: _)
+      case Nil => Right(Nil)
 
-  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = ???
+  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] =
+    traverse(es)(e => e)
 
-  def mean(xs: IndexedSeq[Double]): Either[String, Double] = 
+  def mean(xs: IndexedSeq[Double]): Either[String, Double] =
     if xs.isEmpty then
       Left("mean of empty list!")
-    else 
+    else
       Right(xs.sum / xs.length)
 
-  def safeDiv(x: Int, y: Int): Either[Throwable, Int] = 
+  def safeDiv(x: Int, y: Int): Either[Throwable, Int] =
     try Right(x / y)
     catch case NonFatal(t) => Left(t)
 
@@ -35,8 +52,18 @@ object Either:
     try Right(a)
     catch case NonFatal(t) => Left(t)
 
-  def map2All[E, A, B, C](a: Either[List[E], A], b: Either[List[E], B], f: (A, B) => C): Either[List[E], C] = ???
+  def map2All[E, A, B, C](a: Either[List[E], A], b: Either[List[E], B], f: (A, B) => C): Either[List[E], C] =
+    (a, b) match
+      case (Right(aa), Right(bb)) => Right(f(aa, bb))
+      case (Left(e), Right(_)) => Left(e)
+      case (Right(_), Left(e)) => Left(e)
+      case (Left(e), Left(e2)) => Left(e++e2)
 
-  def traverseAll[E, A, B](as: List[A], f: A => Either[List[E], B]): Either[List[E], List[B]] = ???
 
-  def sequenceAll[E, A](as: List[Either[List[E], A]]): Either[List[E], List[A]] = ???
+  def traverseAll[E, A, B](as: List[A], f: A => Either[List[E], B]): Either[List[E], List[B]] =
+    as.foldRight(Right(Nil): Either[List[E], List[B]]){
+      case (a, acc) => map2All(f(a), acc, _ :: _)
+    }
+
+  def sequenceAll[E, A](as: List[Either[List[E], A]]): Either[List[E], List[A]] =
+    traverseAll(as, identity)
--- src/main/scala/fpinscala/exercises/errorhandling/Option.scala
+++ src/main/scala/fpinscala/exercises/errorhandling/Option.scala
@@ -7,39 +7,82 @@ enum Option[+A]:
   case Some(get: A)
   case None
 
-  def map[B](f: A => B): Option[B] = ???
+  def map[B](f: A => B): Option[B] = this match
+    case None => None
+    case Some(a) => Some(f(a))
 
-  def getOrElse[B>:A](default: => B): B = ???
+  def getOrElse[B>:A](default: => B): B = this match
+    case None => default
+    case Some(a) => a
 
-  def flatMap[B](f: A => Option[B]): Option[B] = ???
+  def flatMap[B](f: A => Option[B]): Option[B] =
+    map(f).getOrElse(None)
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = ???
+  def orElse[B>:A](ob: => Option[B]): Option[B] =
+    map(Some(_)).getOrElse(ob)
 
-  def filter(f: A => Boolean): Option[A] = ???
+  def filter(f: A => Boolean): Option[A] =
+    flatMap(a => if f(a) then Some(a) else None)
 
 object Option:
 
   def failingFn(i: Int): Int =
-    val y: Int = throw new Exception("fail!") // `val y: Int = ...` declares `y` as having type `Int`, and sets it equal to the right hand side of the `=`.
+    // `val y: Int = ...` declares `y` as having type `Int`, and sets it equal to the right hand side of the `=`.
+    val y: Int = throw new Exception("fail!")
     try
       val x = 42 + 5
       x + y
-    catch case e: Exception => 43 // A `catch` block is just a pattern matching block like the ones we've seen. `case e: Exception` is a pattern that matches any `Exception`, and it binds this value to the identifier `e`. The match returns the value 43.
+    // A `catch` block is just a pattern matching block like the ones we've seen. `case e: Exception` is a pattern
+    // that matches any `Exception`, and it binds this value to the identifier `e`. The match returns the value 43.
+    catch case e: Exception => 43
 
   def failingFn2(i: Int): Int =
     try
       val x = 42 + 5
-      x + ((throw new Exception("fail!")): Int) // A thrown Exception can be given any type; here we're annotating it with the type `Int`
+      // A thrown Exception can be given any type; here we're annotating it with the type `Int`
+      x + ((throw new Exception("fail!")): Int)
     catch case e: Exception => 43
 
   def mean(xs: Seq[Double]): Option[Double] =
     if xs.isEmpty then None
     else Some(xs.sum / xs.length)
 
-  def variance(xs: Seq[Double]): Option[Double] = ???
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    a.flatMap(aa => b.map(bb => f(aa, bb)))
 
-  def sequence[A](as: List[Option[A]]): Option[List[A]] = ???
+  def sequence[A](as: List[Option[A]]): Option[List[A]] =
+    sequence_3(as)
 
-  def traverse[A, B](as: List[A])(f: A => Option[B]): Option[List[B]] = ???
+  private def sequence_3[A](as: List[Option[A]]): Option[List[A]] =
+    traverse(as)(as => as)
+
+  private def sequence_1[A](as: List[Option[A]]): Option[List[A]] =
+    as.foldLeft(Some(List.empty[A])){
+      case (Some(acc), Some(a))=> Some(acc:+a)
+      case _ => None
+    }
+
+  private def sequence_2[A](as: List[Option[A]]): Option[List[A]] =
+    as match
+      case Nil => Some(Nil)
+      case ::(head, tail) => head.flatMap(hh => sequence_2(tail).map(hh +: _))
+
+  def traverse[A, B](as: List[A])(f: A => Option[B]): Option[List[B]] =
+    traverse_1(as)(f)
+
+  private def traverse_2[A, B](as: List[A])(f: A => Option[B]): Option[List[B]] =
+    as.foldLeft(Some(List.empty[B])) {
+      case (Some(acc), a) => f(a).map(b => acc :+ b)
+      case _ => None
+    }
+
+  private def traverse_1[A, B](as: List[A])(f: A => Option[B]): Option[List[B]] =
+    as match
+      case Nil => Some(Nil)
+      case ::(head, next) => map2(f(head), traverse_1(next)(f))(_ +: _)
+
+  def lift[A, B](f: A => B): Option[A] => Option[B] =
+    _.map(f)
